78744c44352717e215ebab15882852128e6c2f83
Rishni <ashameemeduma@gmail.com>
2024-03-16 03:27:28 +0530

    added router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,12 @@
 import './bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import { createApp } from 'vue';
 import App from './App.vue';
+import VueAxios from 'axios';
+import router from './router';
 
-createApp(App).mount('#app');
+let app = createApp(App)
+app.use(VueAxios, axios);
+app.use(router, router);
+app.mount('#app');
