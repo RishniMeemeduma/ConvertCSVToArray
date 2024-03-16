e291d98b5d346521d537c297119fcd6531a3c7d5
Rishni <ashameemeduma@gmail.com>
2024-03-17 00:53:43 +0530

    fixed css

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -3,21 +3,100 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\csvFileValidation;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CSVController extends Controller
 {
+    private $success;
+    private $fail;
+
+    public function __construct() {
+        $this->success = 200;
+        $this->fail = 500;
+    }
     /**
      * convert csv to array
      */
-    public function convertCSVToArray(csvFileValidation $request) : string
+    public function convertCSVToArray(csvFileValidation $request) : JsonResponse
     {
-        // dd($request->hasFile("file"));
-        // dd($request->file("file"));
-        // dd($request->file('file')->extension());
-        // if( ) {
-        //     // get the file real path
+        $people = [];
+        // get file path
+        //  read the csv file and parse the content into a multidiamentional array
+        // write conditions to build a new array with key value pair
+        // return the new array
 
-        // }
+        $getFilePath = $request->file('file')->getRealPath();
+        // dd($getFilePath);
+        $csvData = array_filter(array_map('str_getcsv', file($getFilePath)));
+
+        //elemenate the first item
+        unset($csvData[0]);
+        // dd($csvData);
+
+
+        if($csvData && count($csvData) > 0) {
+            foreach($csvData as $key => $owner) {
+                $people[] = $this->splitOwnerStringToArray($owner[0]);
+            }
+            return response()->json(['data' => $people] , $this->success);
+        }
+
+        return response()->json(['No data available in the csv file'], $this->fail);
+        
     }
+
+    /**
+     * @param $ower
+     * return the owner details array
+     */
+    public function splitOwnerStringToArray( string $owner, array $details = []) : array 
+    {
+        if($owner) {
+            // check whether and is available
+            if (str_contains($owner,'and') || str_contains($owner,'&')) {
+
+                $names = str_contains($owner,'and') ? explode('and', $owner, 2) :explode('&', $owner, 2)  ;
+                foreach($names as $key=> $name) {
+                    $name = trim($name);
+                    $ownerNameArray = explode(' ', $name);
+                    $nextOwnerNameArray = $key < count($names) - 1  ? explode(' ', trim($names[$key+1])) : 'null';
+                    
+                    $firstName = count($ownerNameArray) === 3 ? $ownerNameArray[1] : 'null';
+                    $lastName = count($ownerNameArray) > 1 ? end($ownerNameArray) : end($nextOwnerNameArray);
+                    $fullName = $ownerNameArray[0]. ' '.$firstName.' '.$lastName;
+                   
+                    $dataArray[] = $this->splitOwnerStringToArray($fullName, $details);
+                }
+                // dd($dataArray);
+                $details = $dataArray;
+
+            }else {
+                $name = explode(' ', $owner);
+                
+                $nameArray = [
+                    'title' => $name[0],
+                ];
+                
+                if (str_contains($owner,'.')) {
+                    $dot_position = strpos($owner, '.');
+                    
+                    // get the position of dot
+                    $nameArray['first_name'] = !str_contains($name[1], '.') ? $name[1] : 'null';
+                    $nameArray['initial'] = substr($owner, $dot_position -1 , 1 );
+                    $nameArray['last_name'] = end($name) ?? 'null';
+                } else {
+                    $nameArray['first_name'] = $name[1];
+                    $nameArray['last_name'] = $name[2] ?? 'null';
+                }
+
+                $details = $nameArray;
+
+            }
+            
+            return $details;
+        }
+        return [];
+    }
+
 }
--- resources/js/CsvFileConverter.vue
+++ resources/js/CsvFileConverter.vue
@@ -1,13 +1,53 @@
 <template>
-    <div class="container">
-    <!-- Title section -->
-        <div class="title text-center mt-2">
-            <h1> CSV to Array</h1>
-        </div>
+    <div class="csv_file_container" style="">
+        <div class="container">
+        <!-- Title section -->
+            <div class="title text-center mt-2">
+                <h1> CSV to Array Conversion</h1>
+            </div>
 
-    <!-- File Upload section -->
-        <div class="">
-            <input type="file" @change="handleFileUpload">
+        <!-- File Upload section -->
+            <div class="upload_file mt-5">
+                <b>Upload CSV file :  </b>
+                <input type="file" @change="handleFileUpload">
+            </div>
+
+            <div class='alert alert-danger mt-3' v-if="errors.length > 0 || errors['file']" >
+                <ul v-if="errors">
+                    <li v-for="error in errors" v-if="!errors['file']">
+                        {{error}}
+                    </li>
+                    <li v-else>
+                        {{errors['file'][0]}}
+                    </li>
+                </ul>
+            </div>
+
+            <!-- owner data section  -->
+            <div v-else class="mt-5 ">
+                <div class="sub_title text-center"><h2>House Owners Array</h2></div>
+                <div v-if="owners.length > 0" class="d-flex flex-row w-100 flex-wrap mt-3 justify-content-center"> 
+                    
+                    <div  v-for="owner in owners"  class="col-md-4 mb-2 mr-2" style="width:31%; margin-right:1%">
+                        <div v-if="Array.isArray(owner)" class="details_card card mb-2 h-100 p-5">
+                            <div v-for="detail in owner">
+                                $person['title'] => {{ detail['title'] }} <br/>
+                                $person['first_name'] => {{ detail['first_name'] ?? 'null' }}<br/>
+                                $person['initial'] => {{ detail['initial'] ?? 'null' }}<br/>
+                                $person['last_name'] => {{ detail['last_name'] }}<br/>
+                            </div>
+                        </div>
+                        <div v-else class="card mb-2 h-100 p-5 details_card">
+                            $person['title'] => {{ owner['title'] }} <br/>
+                            $person['first_name'] => {{ owner['first_name'] ?? 'null' }}<br/>
+                            $person['initial'] => {{ owner['initial'] ?? 'null' }}<br/>
+                            $person['last_name'] => {{ owner['last_name'] }}<br/>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div v-else class="mt-5 text-center">No data to display</div>
+            </div>
         </div>
     </div>
 </template>
@@ -16,7 +56,9 @@
 
     data() {
         return {
-            file: []
+            file: [],
+            errors: [],
+            owners : [],
         }
     },
     methods: {
@@ -27,17 +69,18 @@
         },
         // send an api request to get the CSV data array
         getDataArray() {
+            this.errors = [];
             const formData = new FormData();
             formData.append('file', this.file);
 
-            this.axios.post('api/upload-csv', formData, {
-                headers: {
-                'Content-Type': 'multipart/form-data'
+            this.axios.post('api/upload-csv', formData)
+            .then((data) => {
+               this.owners = data.data.data;
+            }).catch((error) => {
+                if (error && error.response && error.response.data.errors) {
+                    this.errors = error.response.data.errors
                 }
             })
-            .then((data) => {
-
-            });
         }
 
     },
